e0b45beba83d04624de2361805578e336152eeb9
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-06-29 11:14:03 +0900

    Fix deploy

--- .bin/travis-release.sh
+++ .bin/travis-release.sh
@@ -7,13 +7,11 @@ if [[ $TRAVIS_TAG != "" ]]; then
   exit 0
 fi
 
-if [[ $TRAVIS_PHP_VERSION != $PHP_VERSION_TO_DEPLOY ]]; then
+if [[ $TRAVIS_PHP_VERSION != "$PHP_VERSION_TO_DEPLOY" ]]; then
   echo "TRAVIS_PHP_VERSION=$TRAVIS_PHP_VERSION"
   echo 'Auto release will not be perfomed against tagged the PHP version.'
   exit 0
 fi
-
-BRANCH_TO_DEPLOY="latest"
 
 if [ $TRAVIS_BRANCH != "master" ]; then
   echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
@@ -25,7 +23,7 @@ git config user.name 'kamataryo@travis'
 git config user.email "kamataryo@users.noreply.github.com"
 
 mkdir __dist
-cp -r ./language __dist/
+cp -r ./languages __dist/
 cp -r ./vendor __dist/
 cp readme.txt __dist/
 cp screenshot-*.png __dist/
@@ -36,6 +34,5 @@ git init
 git remote add origin git@github.com:$GH_REF.git
 git add .
 git commit -m"Release from Travis CI[ci skip]"
-git checkout -b $BRANCH_TO_DEPLOY
-git push origin :$BRANCH_TO_DEPLOY
-git push origin $BRANCH_TO_DEPLOY
+git checkout -b $BRANCH_TO_RELEASE
+git push -f origin $BRANCH_TO_RELEASE
